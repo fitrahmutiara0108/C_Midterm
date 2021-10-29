3056fd75de8576d027f1f528e8e618200e4d0b6e
fitrahmutiara0108 <fitrahmutiara.19051@mhs.its.ac.id>
2021-10-30 01:25:44 +0700

    tolong saya ya Allah

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# IF184504_WEBPRO_C_Midterm
+# Final Project
+
+## Kelompok 5
+
+-   GALIH S OKTAVIANTO (@uselessahar)
+-   Samuel (@samuelsih)
+-   Alfarizqi Abiyyu (@alfarizqi_abiyyu)
+
+## Tema Project
+
+### Musikpedia penyedia layanan rating untuk musik
+
+## ERD
+
+![](/public/image/erd.png)
+
+##Link Video
+Link Demo Aplikasi : https://youtu.be/RXJOQ7B8u1o
+Link Deploy : http://final-project-laravel-sanber.herokuapp.com/
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit13c8af8e2c96e36c9647a06ace5b9485
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit13c8af8e2c96e36c9647a06ace5b9485', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit13c8af8e2c96e36c9647a06ace5b9485', 'loadClassLoader'));
